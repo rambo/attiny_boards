6732412118018422c46f716c830c43e272628283
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-08-20 22:37:13 +0300

    added reminder of the xtal pins

--- attiny85_blink/attiny85_blink.ino
+++ attiny85_blink/attiny85_blink.ino
@@ -6,25 +6,27 @@ arduino pin 0 = not(OC1A) = PORTB <- _BV(0) = SOIC pin 5 (I2C SDA, PWM)
 arduino pin 2 =           = PORTB <- _BV(2) = SOIC pin 7 (I2C SCL, Analog 1)
 // Timer1 -> PWM
 arduino pin 1 =     OC1A  = PORTB <- _BV(1) = SOIC pin 6 (PWM)
-arduino pin 3 = not(OC1B) = PORTB <- _BV(3) = SOIC pin 2 (Analog 3)
-arduino pin 4 =     OC1B  = PORTB <- _BV(4) = SOIC pin 3 (Analog 2)
+arduino pin 3 = not(OC1B) = PORTB <- _BV(3) = SOIC pin 2 (Analog 3, XTAL)
+arduino pin 4 =     OC1B  = PORTB <- _BV(4) = SOIC pin 3 (Analog 2, XTAL)
  */
+
+#define BLINKPIN 3 // Note: if you have xtal you can't use this as output
 
 void setup()
 {
     // TODO: Tri-state this and wait for input voltage to stabilize 
-    pinMode(3, OUTPUT); // OC1B-, Arduino pin 3, ADC
-    digitalWrite(3, LOW); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
+    pinMode(BLINKPIN, OUTPUT); // OC1B-, Arduino pin 3, ADC
+    digitalWrite(BLINKPIN, LOW); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
     
     // Whatever other setup routines ?
     
-    digitalWrite(3, HIGH);
+    digitalWrite(BLINKPIN, HIGH);
 }
 
 void loop()
 {
-    digitalWrite(3, LOW); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
+    digitalWrite(BLINKPIN, LOW); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
     delay(500);
-    digitalWrite(3, HIGH);
+    digitalWrite(BLINKPIN, HIGH);
     delay(500);
 }
